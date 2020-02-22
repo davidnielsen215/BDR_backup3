bfca2e78b230aae9bf2b65d30084998a5e3913f8
David Nielsen <davidnielsen215@gmail.com>
2020-02-21 18:30:40 -0700

    added comments for referencing recurly api

--- client/src/main/Subscriptions/SubscriptionList.js
+++ client/src/main/Subscriptions/SubscriptionList.js
@@ -4,6 +4,8 @@ import AddSubscriptionFormContainer from "./AddSubscriptionFormContainer";
 
 //BORDER COLOR = RED
 
+//The subscription container will return a component called subscription and will have edit
+//and delete functions that are called in the redux store
 function SubscriptionList(props) {
     const subscriptions = props.subscriptions.map(subscription => {
         return (
@@ -14,6 +16,9 @@ function SubscriptionList(props) {
         )
     })
 
+   // The addSubscriptionFormContainer is what is wrapped around all of the components
+   // within this component is the AddSubscription component
+   //this is where the subscriptions are created in the redux store and then sent to the DB
     return (
         <div style={{border: '5px solid lime'}}>
             <AddSubscriptionFormContainer/>
